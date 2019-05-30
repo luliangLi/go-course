133843a0f636982dc6576ab46a1de78dfec092cb
Li, Luliang <luliang.li@anz.com>
2019-05-30 15:11:32 +1000

    add insert sort

--- 02_bubble/luliangLi/main.go
+++ 02_bubble/luliangLi/main.go
@@ -16,9 +16,25 @@ func bubble(s []int) []int {
 	for i := 0; i < len(s)-1; i++ {
 		for j := 0; j < len(s)-1-i; j++ {
 			if s[j] > s[j+1] {
-				temp := s[j]
-				s[j] = s[j+1]
-				s[j+1] = temp
+				s[j], s[j+1] = s[j+1],s[j]
+			}
+		}
+	}
+
+	return s
+}
+
+func insertSort(s []int) []int {
+	if s == nil || len(s) <= 1 {
+		return s
+	}
+
+	for i := 1; i < len(s); i++ {
+		for j := i - 1; j >= 0; j-- {
+			if s[j] > s[j+1] {
+				s[j], s[j+1] = s[j+1],s[j]
+			}else{
+				break
 			}
 		}
 	}
--- 02_bubble/luliangLi/main_test.go
+++ 02_bubble/luliangLi/main_test.go
@@ -16,6 +16,7 @@ func TestBubbleOutput(t *testing.T) {
 		"normal": {input: []int{3, 2, 1, 5}, want: []int{1, 2, 3, 5}},
 		"blank":  {input: []int{}, want: []int{}},
 		"nil":    {input: nil, want: nil},
+		"duplicated":    {input: []int{1, 0, 0, 1}, want: []int{0, 0, 1, 1}},
 	}
 
 	for name, test := range testCases {
@@ -24,6 +25,32 @@ func TestBubbleOutput(t *testing.T) {
 
 		t.Run(name, func(t *testing.T) {
 			actual := bubble(test.input)
+
+			if !reflect.DeepEqual(test.want, actual) {
+				t.Errorf("running : %v, expected %v, got %v", test.input, test.want, actual)
+			}
+		})
+	}
+}
+
+func TestInsertSortOutput(t *testing.T) {
+
+	testCases := map[string]struct {
+		input []int
+		want  []int
+	}{
+		"normal": {input: []int{3, 2, 1, 5}, want: []int{1, 2, 3, 5}},
+		"blank":  {input: []int{}, want: []int{}},
+		"nil":    {input: nil, want: nil},
+		"duplicated":    {input: []int{1, 0, 0, 1}, want: []int{0, 0, 1, 1}},
+	}
+
+	for name, test := range testCases {
+		// t.Run creates a sub test and runs it like a normal test
+		test := test
+
+		t.Run(name, func(t *testing.T) {
+			actual := insertSort(test.input)
 
 			if !reflect.DeepEqual(test.want, actual) {
 				t.Errorf("running : %v, expected %v, got %v", test.input, test.want, actual)
